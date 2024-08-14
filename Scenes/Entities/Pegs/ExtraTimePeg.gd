138137a7828bab61f2ee0dca028be04b3ee32b5a
Megarion <andy181209x@gmail.com>
2024-08-14 19:56:48 +0700

    Update ExtraTimePeg.gd

--- Scenes/Entities/Pegs/ExtraTimePeg.gd
+++ Scenes/Entities/Pegs/ExtraTimePeg.gd
@@ -9,4 +9,3 @@ func addlabel() -> void:
 func endtrigger(body) -> void:
 	var final = extraTime * Global.time_peg_effectiveness_multiplier
 	Global.GAME_VAR.timer += final
-	Global.STATS["total_time"] += final
